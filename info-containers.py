179ebcf96c53207db88cc9651afa767833b064b6
Fabio Falci <fabiofalci@gmail.com>
2015-07-10 17:30:03 +0100

    Print number of containers

--- info-containers.py
+++ info-containers.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import os
 
 docker_path = "/var/lib/docker"
 containers_path = docker_path + "/containers"
@@ -9,11 +10,20 @@ class Containers:
     def __init__(self, path):
         self.path = path
 
+    def get_number_of_containers(self):
+        number_of_containers = 0
+        for _ in os.listdir(self.path):
+            number_of_containers += 1
+
+        return number_of_containers
+
+
 
 def main():
     print("Container info")
 
     containers = Containers(containers_path)
+    print("Number of containers ", containers.get_number_of_containers())
 
 
 if __name__ == "__main__":
